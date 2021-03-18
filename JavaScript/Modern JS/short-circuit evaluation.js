570c4745df613b8a449fad03f055b897d73b8e96
jiyoungbkim <jiyoungbkim@gmail.com>
2021-03-18 22:55:03 +0900

    short-circuit evaluation

---
+++ JavaScript/Modern JS/short-circuit evaluation.js
@@ -0,0 +1,26 @@
+true && true // true
+true && false // false
+true || false // true
+false || true // true
+
+console.log(true && 'hello'); // hello
+console.log(false && 'hello'); // false
+console.log('hello' && 'bye'); // bye
+console.log(null && 'hello'); // null
+console.log(undefined && 'hello'); // undefined
+console.log('' && 'hello'); // ''
+console.log(0 && 'hello'); // 0
+console.log(1 && 'hello'); // hello
+console.log(1 && 1); // 1
+
+const namelessDog = {
+    name: ''
+};
+
+function getName(animal) {
+    const name = animal && animal.name;
+    return name || '이름이 없는 동물입니다.';
+}
+
+const name = getName(namelessDog);
+console.log(name); // 이름이 없는 동물입니다.
