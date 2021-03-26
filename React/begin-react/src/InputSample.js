c373bb2e0cc636d5d7ebe40337ec28537bf72ca4
jiyoungbkim <jiyoungbkim@gmail.com>
2021-03-26 22:39:36 +0900

    useState Object

---
+++ React/begin-react/src/InputSample.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function InputSample() { 
+  const [inputs, setInputs] = useState ({
+    name: '',
+    nickname: ''
+  });
+  
+  const {name, nickname} = inputs;
+
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name] : value
+    })
+  }
+
+  const onReset = () => {
+    setInputs({
+      name: '',
+      nickname: ''
+    })
+  }
+  return (
+    <div>
+      <input 
+        name="name" 
+        placeholder="name" 
+        onChange={onChange} 
+        value={name}
+      />
+      <input 
+        name= "nickname" 
+        placeholder="nickname" 
+        onChange={onChange} 
+        value={nickname}
+      />
+      <button onClick={onReset}>Reset</button>
+      <div>
+        <b>value: </b>
+        {name} ({nickname})
+      </div>
+    </div>
+  )
+}
+
+export default InputSample;
