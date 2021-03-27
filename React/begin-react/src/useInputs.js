9ffa2d9f038bb169b1c3c9b918c5011df917cc4d
jiyoungbkim <jiyoungbkim@gmail.com>
2021-03-28 00:39:26 +0900

    custom Hook_useReducer

--- React/begin-react/src/useInputs.js
+++ React/begin-react/src/useInputs.js
@@ -1,14 +1,41 @@
-import { useState, useCallback } from 'react';
+import { useReducer , useCallback } from 'react';
 
+function reducer (state, action ){
+  switch (action.type) {
+    case 'CHANGE' :
+      return {
+        ...state,
+        [action.name] : action.value        
+      };
+    case 'RESET' :
+      return Object.keys(state).reduce((acc, current) =>{
+         acc[current] = '';
+         return acc;
+      }, {});
+    default:
+      throw new Error('Unhandled action');
+  }
+
+}
 function useInputs(initialForm) {
-  const [form, setForm] = useState(initialForm);
+  const [state, dispatch] = useReducer(reducer, initialForm);
+  
   const onChange = useCallback(e => {
     const { name, value } = e.target;
-    setForm(form => ({ ...form, [name]: value}));
+    dispatch({
+      type: 'CHANGE',
+      name,
+      value
+    });
   }, []);
-  const reset = useCallback (() => setForm(initialForm), [initialForm]);
 
-  return [form, onChange, reset];
+  const reset = useCallback(() => {
+    dispatch({
+      type: 'RESET'
+    })
+  }, []);
+
+  return [state, onChange, reset];
 };
 
 export default useInputs;
