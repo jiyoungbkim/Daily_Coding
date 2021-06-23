99d5fd04f5ec15ef9bb18addf863f36a829632a9
jiyoungbkim <jiyoungbkim@gmail.com>
2021-06-23 23:00:05 +0900

    React Hooks

---
+++ React/react-hooks/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import InputApp from "./UseState/InputApp";
+
+function App() {
+  return (
+    <div className="App">
+      <InputApp />
+    </div>
+  );
+}
+
+export default App;
