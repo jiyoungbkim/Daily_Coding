ace377dd5b815dfcca4c1015a46bcfc1b892e6d6
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-19 22:43:09 +0900

    vue computed : getter, setter

--- Vue/vue3-computed/src/App.vue
+++ Vue/vue3-computed/src/App.vue
@@ -1,13 +1,44 @@
 <template>
-  <Fruits />
+  <button @click="add">
+    ADD
+  </button>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
+  <h1>{{ reversedMessage }}</h1>
 </template>
 
 <script>
-import Fruits from '~/components/Fruits'
-
 export default {
-  components: {
-    Fruits
+  data() {
+    return {
+      // Getter, Setter
+      msg: 'Hello Computed!'
+    }
+  },
+  // 캐싱 
+  computed: {
+    // 읽기 전용 Getter
+    // reversedMessage() {
+    //   return this.msg.split('').reverse().join('')
+    // }
+    // Getter, Setter
+    reversedMessage: {
+      get() {
+        return this.msg.split('').reverse().join('')
+      }, 
+      set(value) {
+        this.msg = value    
+      }
+    }
+  },
+  methods: {
+    // reverseMessage() {
+    //   return this.msg.split('').reverse().join('')
+    // }
+    add() {
+      this.reversedMessage += '!?'
+    }
   }
 }
 </script>
--- Vue/vue3-computed/src/components/Fruits.vue
+++ Vue/vue3-computed/src/components/Fruits.vue
@@ -45,5 +45,4 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
 </style>
