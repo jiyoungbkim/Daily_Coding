869213bce471307cd37813f79034e70cf5092e3b
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-20 22:08:57 +0900

    vue : watch

--- Vue/vue3-computed/src/App.vue
+++ Vue/vue3-computed/src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <button @click="add">
-    ADD
-  </button>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
+  <div>
+    <!-- <button @click="add">
+      ADD
+    </button> -->
+    <h1 @click="changeMessage">
+      {{ msg }}
+    </h1>
+    <h1>
+      {{ reversedMessage }}
+    </h1>  
+  </div>
 </template>
 
 <script>
@@ -13,32 +17,46 @@ export default {
   data() {
     return {
       // Getter, Setter
-      msg: 'Hello Computed!'
+      msg: 'Hello?',
+      isActive: false
     }
   },
   // 캐싱 
   computed: {
     // 읽기 전용 Getter
-    // reversedMessage() {
-    //   return this.msg.split('').reverse().join('')
-    // }
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
     // Getter, Setter
-    reversedMessage: {
-      get() {
-        return this.msg.split('').reverse().join('')
-      }, 
-      set(value) {
-        this.msg = value    
-      }
+    // reversedMessage: {
+    //   get() {
+    //     return this.msg.split('').reverse().join('')
+    //   }, 
+    //   set(value) {
+    //     this.msg = value    
+    //   }
+    // }
+  },
+  // 데이터 변경 확인
+  watch: {
+    msg() {
+      console.log('msg:', this.msg)
+    },
+    reversedMessage() {
+      console.log('reversedMessage', this.reversedMessage)
     }
   },
   methods: {
     // reverseMessage() {
     //   return this.msg.split('').reverse().join('')
     // }
-    add() {
-      this.reversedMessage += '!?'
-    }
+    // add() {
+    //   this.reversedMessage += '!?'
+    // },
+    changeMessage() {
+      this.msg = "Good!"
+    },
+    
   }
 }
 </script>
