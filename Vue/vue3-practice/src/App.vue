8480ff34026d6fbc149cd07b76e197bc67fcc00d
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-20 22:28:17 +0900

    class, style binding

---
+++ Vue/vue3-practice/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h1
+    :class="{active: isActive}"
+    @click="activate">
+    Hello({{ isActive }}) 
+  </h1>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    activate() {
+      this.isActive = true
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  color: red;
+  font-weight: bold;
+}
+</style>
