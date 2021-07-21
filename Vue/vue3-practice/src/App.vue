b9b874aa1af2471c0f3fdc786f18bf22dd8d97d1
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-21 22:19:56 +0900

    리스트 렌더링

--- Vue/vue3-practice/src/App.vue
+++ Vue/vue3-practice/src/App.vue
@@ -1,42 +1,43 @@
 <template>
   <div>
     <button @click="handler">
-      click me!
+      Click me!
     </button>
-    <!-- <h1 v-if="isShow">
-      Hello?
-    </h1>
-    <h1 v-else-if="count > 3">
-      Count > 3
-    </h1>
-    <h1 v-else>
-      Good~
-    </h1> -->
-    <!-- 최상위 템플릿에서는 v-if를 사용할 수 없지만 하위 템플릿에는 사용가능. 의미없는 div 대신 사용 -->
-    <template v-if="isShow"> 
-      <h1>Title</h1>
-      <p>Paragraph 1</p>
-      <p>Paragraph 2</p>
-    </template>
-    <!-- v-if가 false일 경우 요소 자체도 렌더링하지 않는다 v-show일 때는 display: none으로 렌더링-->
-    <h1 v-show="isShow">
-      Hello?!
-    </h1>
+    <ul>
+      <!-- <li
+      v-for="(fruit, index) in fruits"
+      :key="fruit">
+      {{ fruit }}-{{ index + 1 }}
+    </li> -->
+      <li
+        v-for="{id, name} in newFruits"
+        :key="id">
+        {{ name }}-{{ id }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import shortid from 'shortid'
+
 export default {
   data() {
     return {
-      isShow: true,
-      count: 0
+      fruits: ['Apple', 'Banana', 'Cherry']
+    }
+  },
+  computed: {
+    // 배열 데이터로 새로 만드는 객체 배열
+    newFruits() {
+      return this.fruits.map((fruit) => ({
+        id: shortid.generate(),
+        name: fruit}))
     }
   },
   methods: {
     handler() {
-      this.isShow = !this.isShow
-      this.count += 1
+      this.fruits.push('Orange')
     }
   }
 }
