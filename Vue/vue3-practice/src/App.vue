56ca0dff67e5b80804cf6dbb97e9bdf0ed2b22f2
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-25 16:40:23 +0900

    event handling - key

--- Vue/vue3-practice/src/App.vue
+++ Vue/vue3-practice/src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <a
-    href="https://naver.com"
-    target="_blank"
-    @click.prevent.once="handler">
-    <!-- @click.prevent="handler"> -->
-    NAVER
-  </a>
+  <input
+    type="text"
+    @keydown.ctrl.a="handler" />
+    <!-- 키의 조합을 체이닝으로 ctrl + a를 눌렀을 때 출력 -->
+    <!-- @keydown.enter="handler" /> -->
 </template>
 
 <script>
   export default {
     methods: {
       handler() {
-        // html 기본 기능 동작 막기
-        // event.preventDefault()
-        console.log('ABC!!!')
+        // if(event.key === 'Enter'){
+          console.log('Enter!!')
+        // }
       }
     }
   }
