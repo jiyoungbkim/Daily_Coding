c1a5bda18f1dd426f01d0f1f69b6d0ca8cf1f96b
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-25 16:31:42 +0900

    event handling

--- Vue/vue3-practice/src/App.vue
+++ Vue/vue3-practice/src/App.vue
@@ -1,44 +1,21 @@
 <template>
-  <div>
-    <button @click="handler">
-      Click me!
-    </button>
-    <ul>
-      <!-- <li
-      v-for="(fruit, index) in fruits"
-      :key="fruit">
-      {{ fruit }}-{{ index + 1 }}
-    </li> -->
-      <li
-        v-for="{id, name} in newFruits"
-        :key="id">
-        {{ name }}-{{ id }}
-      </li>
-    </ul>
-  </div>
+  <a
+    href="https://naver.com"
+    target="_blank"
+    @click.prevent.once="handler">
+    <!-- @click.prevent="handler"> -->
+    NAVER
+  </a>
 </template>
 
 <script>
-import shortid from 'shortid'
-
-export default {
-  data() {
-    return {
-      fruits: ['Apple', 'Banana', 'Cherry']
-    }
-  },
-  computed: {
-    // 배열 데이터로 새로 만드는 객체 배열
-    newFruits() {
-      return this.fruits.map((fruit) => ({
-        id: shortid.generate(),
-        name: fruit}))
-    }
-  },
-  methods: {
-    handler() {
-      this.fruits.push('Orange')
+  export default {
+    methods: {
+      handler() {
+        // html 기본 기능 동작 막기
+        // event.preventDefault()
+        console.log('ABC!!!')
+      }
     }
   }
-}
 </script>
