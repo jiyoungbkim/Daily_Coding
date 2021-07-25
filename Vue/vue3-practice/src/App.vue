53b1f446835aa3beed5b8bab88825e9f835181bd
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-25 21:07:11 +0900

    component basic

--- Vue/vue3-practice/src/App.vue
+++ Vue/vue3-practice/src/App.vue
@@ -1,25 +1,13 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <input
-    type="text"
-    v-model.trim="msg" />
-    <!-- v-model.lazy="msg" /> -->
-    <!-- 값이 바뀌고 나서 엔터, 탭, 바깥쪽 클릭시 갱신 -->
-    <!-- :value="msg" -->
-    <!-- @change="msg = $event.target.value" /> -->
+  <Basic />
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        msg: 'Hello'
-      }
-    },
-    watch: {
-      msg() {
-        // console.log(this.msg.trim())
-      }
-    }
+import Basic from '~/Basic'
+
+export default {
+  components: {
+    Basic
   }
+}
 </script>
