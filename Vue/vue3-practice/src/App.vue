ab45f725faf5d7a7f3cf569129542d70fa371da8
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-25 16:59:55 +0900

    폼 입력 바인딩

--- Vue/vue3-practice/src/App.vue
+++ Vue/vue3-practice/src/App.vue
@@ -1,19 +1,33 @@
 <template>
+  <h1>{{ msg }}</h1>
   <input
     type="text"
-    @keydown.ctrl.a="handler" />
-    <!-- 키의 조합을 체이닝으로 ctrl + a를 눌렀을 때 출력 -->
-    <!-- @keydown.enter="handler" /> -->
+    v-model="msg" />
+  <!-- 한글일 경우는 v-model을 쓰면 한글자 완성이 되야 적용되므로 아래와 같이 작성한다 -->
+  <!-- :value="msg"  -->
+  <!-- @input="msg = $event.target.value" /> -->
+
+  <!-- @input="handler" /> -->
+
+  <h1>{{ checked }}</h1>
+  <input
+    type="checkbox"
+    v-model="checked" />
 </template>
 
 <script>
   export default {
-    methods: {
-      handler() {
-        // if(event.key === 'Enter'){
-          console.log('Enter!!')
-        // }
+    data() {
+      return {
+        msg: 'Hello world!',
+        checked: false
       }
-    }
+    },
+    // methods: {
+    //   handler(event) {
+    //     console.log(event.target.value)
+    //     this.msg = event.target.value
+    //   }
+    // }
   }
 </script>
