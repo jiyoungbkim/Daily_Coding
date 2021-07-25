f5b7a9fdf6194055c704c6c13aa00735660591d6
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-25 21:55:00 +0900

    Emit

--- Vue/vue3-practice/src/App.vue
+++ Vue/vue3-practice/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <!-- <Basic /> -->
-  <Attribute />
+  <!-- <Attribute /> -->
+  <Emit />
 </template>
 
 <script>
 // import Basic from '~/Basic'
-import Attribute from './Attribute.vue'
+// import Attribute from './Attribute.vue'
+import Emit from './Emit.vue'
 
 export default {
   components: {
     // Basic,
-    Attribute
+    // Attribute,
+    Emit
   }
 }
 </script>
---
+++ Vue/vue3-practice/src/Emit.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- 연결하는 이벤트의 이름은 꼭 click이 아니어도 된다 -->
+  <MyBtn
+    @click="log"
+    @change-msg="logMsg">
+    Banana
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/MyBtn-emit'
+
+export default {
+  components: {
+    MyBtn
+  },
+  methods: {
+    log(event) {
+      console.log('Click!')
+      console.log(event)
+    },
+    logMsg(msg) {
+      console.log(msg)
+    }
+  }
+}
+</script>
