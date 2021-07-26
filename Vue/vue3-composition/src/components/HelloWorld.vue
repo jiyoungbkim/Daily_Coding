133065d34b4c8a9176105c9b160ff8ce41f9e3a8
jiyoungbkim <jiyoungbkim@gmail.com>
2021-07-26 22:20:05 +0900

    Composition API : ref value

---
+++ Vue/vue3-composition/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <div @click="increase">
+    {{ count }}
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  // data() {
+  //   return {
+  //     count: 0
+  //   }
+  // },
+  // methods: {
+  //   increase() {
+  //     this.count += 1
+  //   }
+  // },
+  setup() {
+    let count = ref(0)
+    function increase() {
+      count.value += 1
+    }
+
+    return {
+      count,
+      increase
+    }
+  }
+}
+</script>
